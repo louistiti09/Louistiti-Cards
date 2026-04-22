c703430800a63b548ffcbe78baa1efcedb18eeda
LOUISTITI <149299736+louistiti09@users.noreply.github.com>
2026-04-22 21:09:10 +0200

    login update

---
+++ server.js
@@ -0,0 +1,30 @@
+const client = supabase.createClient(
+    "https://ytepoythtrlssufpqnac.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0ZXBveXRodHJsc3N1ZnBxbmFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NzE3MTgsImV4cCI6MjA5MjQ0NzcxOH0.-9iwxSnvCdq0cNnfJ4sHkQX8lCcAulzO2zDzRG8oue4"
+)
+
+async function login() {
+    const { error } = await client.auth.signInWithOAuth({
+        provider: "discord"
+    });
+    if (error) console.error("Login error:", error.message);
+}
+
+async function getUser() {
+    const { data } = await client.auth.getUser();
+    if (!data.user) return null;
+    return {
+        id: data.user.id,
+        username: data.user.user_metadata.full_name,
+        avatar: data.user.user_metadata.avatar_url
+    };
+}
+
+window.onload = async () => {
+    const user = await getUser();
+    if (user) {
+        console.log("Connecté :", user);
+    } else {
+        console.log("Pas connecté");
+    }
+};
