ae11023a15904f0f76d1363d41862f50b110dcd3
LOUISTITI <149299736+louistiti09@users.noreply.github.com>
2026-04-23 20:02:59 +0200

    Classeur

--- server.js
+++ server.js
@@ -49,14 +49,6 @@ async function pay(amount) {
 }
 
 window.onload = async () => {
-    //Head Setup
-    document.head.innerHTML += `
-        <link rel="icon" href="images/Logo.png" type="image/png">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
-    `;
     //Navbar Setup
     document.body.innerHTML += `
         <nav>
@@ -103,4 +95,5 @@ window.onload = async () => {
             <div id="need-login">Veuillez vous connecter.</div>
         `;
     }
+    $(".flipbook").turn();
 };
