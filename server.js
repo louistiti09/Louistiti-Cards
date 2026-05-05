66906beb437792af1b8c7b62879ba8179b211d02
LOUISTITI <149299736+louistiti09@users.noreply.github.com>
2026-05-06 01:12:59 +0200

    Revente

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const client = supabase.createClient(
+export const client = supabase.createClient(
     "https://ytepoythtrlssufpqnac.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0ZXBveXRodHJsc3N1ZnBxbmFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NzE3MTgsImV4cCI6MjA5MjQ0NzcxOH0.-9iwxSnvCdq0cNnfJ4sHkQX8lCcAulzO2zDzRG8oue4"
-)
+);
+
+export let connected = false;
 
 async function login() {
     const { error } = await client.auth.signInWithOAuth({
@@ -16,30 +18,35 @@ async function logout() {
         console.error("Logout error:", error.message);
         return;
     }
+    window.location.href = "index.html"; // Redirige vers la page d'accueil après la déconnexion
     window.location.reload();
 }
 
-async function getUser() {
-    const { data } = await client.auth.getUser();
-    if (!data.user) return null;
+export async function getUser() {
+    connected = false;
+    const { data: sessionData, error: sessionError } = await client.auth.getSession();
+    if (sessionError || !sessionData.session?.user) return null;
+    const user = sessionData.session.user;
     const { data: profile, error } = await client
         .from("user_profiles")
-        .select("credits")
-        .eq("id", data.user.id)
+        .select("credits, confiance")
+        .eq("id", user.id)
         .single();
     if (error) {
         console.error(error);
         return null;
     }
+    connected = true;
     return {
-        id: data.user.id,
-        username: data.user.user_metadata.full_name,
-        avatar: data.user.user_metadata.avatar_url,
-        credits: profile?.credits ?? 0
+        id: user.id,
+        username: user.user_metadata.full_name,
+        avatar: user.user_metadata.avatar_url,
+        credits: profile?.credits ?? 0,
+        confiance: profile?.confiance ?? 0
     };
 }
 
-async function pay(amount) {
+export async function pay(amount) {
     const { error } = await client.rpc("spend_credits", {
         amount: amount
     });
@@ -48,7 +55,38 @@ async function pay(amount) {
     }
 }
 
-window.onload = async () => {
+export async function setup() {
+    //User Setup
+    var ButtonsHTML = "";
+    var AccountHTML = "";
+    const user = await getUser();
+    if (user) {
+        ButtonsHTML = `
+            <a href="revente.html"><button>Revendre</button></a>
+            <a href="classeur.html"><button>Classeur</button></a>
+            <a href="credits.html"><button>Credits</button></a>
+        `;
+        AccountHTML = `
+            <div id="credits">
+                <strong>${user.username}</strong>
+                <br>${user.credits} Crédits
+            </div>
+            <div id='pfp'>
+                <img id='avatar' src='${user.avatar}'>
+                <img id='logout' src='images/LogOut.png'>
+            </div>
+        `;
+    } else {
+        ButtonsHTML = `
+            <button class="disabled">Revendre</button>
+            <a href="classeur.html"><button>Classeur</button></a>
+            <button class="disabled">Credits</button>
+            <div id="need-login">Veuillez vous connecter.</div>
+        `;
+        AccountHTML = `
+            <button id="login">Login</button>
+        `;
+    }
     //Navbar Setup
     document.body.innerHTML += `
         <nav>
@@ -57,42 +95,19 @@ window.onload = async () => {
                 <h2>Louistiti Cards</h2>
             </div>
             
-            <div id="navbar"></div>
+            <div id="navbar">${ButtonsHTML}</div>
 
-            <div id="account">
-                <button onclick="login()">Login</button>
-            </div>
+            <div id="account">${AccountHTML}</div>
         </nav>
         <footer>
             <a href="https://www.cardmarket.com/fr/YuGiOh/Users/LOUISTITI"><img src="images/Cardmarket.png" height="50px"></a>
             <a href="https://discord.gg/ct97JAZdRN"><img src="images/Discord.png" height="50px"></a>
         </footer>
     `;
-    //User Setup
-    const user = await getUser();
-    //const user = {avatar: "https://cdn.discordapp.com/avatars/1018922052620660776/35adbfcef0b504815da738983959f0f5.png",credits: 0,id: "3a4efe60-2cf5-4ecf-b275-323d6fbc571b",username: "louistiti09"};
+    //Connect buttons
     if (user) {
-        document.getElementById("account").innerHTML = `
-            <div id="credits">
-                <strong>${user.username}</strong>
-                <br>${user.credits} Crédits
-            </div>
-            <div id='pfp' onclick="logout()">
-                <img id='avatar' src='${user.avatar}'>
-                <img id='logout' src='images/LogOut.png'>
-            </div>
-        `;
-        document.getElementById("navbar").innerHTML = `
-            <a href="revente.html"><button>Revendre</button></a>
-            <a href="classeur.html"><button>Classeur</button></a>
-            <a href="credits.html"><button>Credits</button></a>
-        `;
+        document.getElementById("pfp").addEventListener("click", logout);
     } else {
-        document.getElementById("navbar").innerHTML = `
-            <button class="disabled">Revendre</button>
-            <a href="classeur.html"><button>Classeur</button></a>
-            <button class="disabled">Credits</button>
-            <div id="need-login">Veuillez vous connecter.</div>
-        `;
+        document.getElementById("login").addEventListener("click", login);
     }
 };
